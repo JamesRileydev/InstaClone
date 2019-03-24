b173ea7aec0b789680c5a75fecbc9b95a68b1e36
JamesRileydev <jamesriley.dev@gmail.com>
2019-03-23 20:34:31 -0400

    Rendering posts

--- src/components/PostContainer/Post.js
+++ src/components/PostContainer/Post.js
@@ -4,11 +4,22 @@ import PropTypes from 'prop-types';
 import "./PostList.css";
 
 const Post = props => {
-    return(
-    <div className="postCard">
-        <h3>{props.post.username}</h3>
+    return (
+    <div>
+        <div>{props.post.username}</div>
+        <div>{props.post.id}</div>
+        <div>{props.post.likes}</div>
+
     </div>
     )
 }
 
+Post.propTypes = {
+    post: PropTypes.shape({
+        id: PropTypes.string,
+        username: PropTypes.string,
+        likes: PropTypes.number,
+        comments: PropTypes.array
+    })
+};
 export default Post;
--- src/components/PostContainer/PostList.js
+++ src/components/PostContainer/PostList.js
@@ -7,12 +7,11 @@ import Post from './Post';
 
 const PostList = props => {
     return (
-        <div>Posts List</div>
-        // <div className="list">
-        // {props.posts.map(post => (
-        //     <Post key={post.created} post={post} />
-        // ))}
-        // </div>
+        <div className="list">
+            {props.posts.map(post => (
+                <Post key={post.id} post={post} />
+            ))}
+        </div>
     );
 };
 
